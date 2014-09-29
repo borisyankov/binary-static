f9c1dda6c6889a68923f6591e1df5fb2d62658fe
Boris Yankov <borisyankov@gmail.com>
2014-09-29 11:35:35 +0800

    Replace compass with native git-sass for less dependencies when building

---
+++ build/sass.js
@@ -0,0 +1,10 @@
+module.exports = {
+    all: {
+        options: {
+            style: 'expanded'
+        },
+        files: {
+            'dist/css/binary.css': 'src/sass/binary.scss',
+        }
+    }
+};
