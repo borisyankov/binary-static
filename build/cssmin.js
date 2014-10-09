44e12d4f5340d7b58e67c6e8cdb19be703219354
Boris Yankov <borisyankov@gmail.com>
2014-10-09 17:27:50 +0800

    Cleanup unused jQuery Theme images

--- build/cssmin.js
+++ build/cssmin.js
@@ -1,8 +1,8 @@
 module.exports = {
     all: {
         files: {
-            'dist/css/binary.min.css': ['src/css/external/**/*.css', 'dist/css/binary.css'],
-            'dist/css/binary_rtl.min.css': ['src/css/external/**/*.css', 'dist/css/binary_rtl.css']
+            'dist/css/binary.min.css': ['src/css/**/*.css', 'dist/css/binary.css'],
+            'dist/css/binary_rtl.min.css': ['src/css/**/*.css', 'dist/css/binary_rtl.css']
         }
     }
 };
