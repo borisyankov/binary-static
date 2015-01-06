52220549efaf3c18a77faab589135d08b3459e74
Boris Yankov <borisyankov@gmail.com>
2015-01-06 04:37:51 +0200

    Fix autoprefixer in build process

--- build/autoprefixer.js
+++ build/autoprefixer.js
@@ -1,10 +1,10 @@
 module.exports = {
     all: {
+        files: {
+            'dist/css/binary.css': 'dist/css/binary.css'
+        },
         options: {
             browsers: ['last 2 versions']
-        },
-        single_file: {
-            src: 'dist/css/binary.css'
         }
     }
 };
