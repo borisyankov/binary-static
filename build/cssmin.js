f72e762ce20066d65553c56b1951c9c29addf16b
Boris Yankov <borisyankov@gmail.com>
2015-01-13 08:17:19 +0200

    Small style fixes

--- build/cssmin.js
+++ build/cssmin.js
@@ -1,7 +1,8 @@
 module.exports = {
     all: {
         files: {
-            'dist/css/binary.min.css': ['src/css/**/*.css', 'dist/css/binary.css'],
+            '../tech-blog/binary.min.css': ['src/css/**/*.css', 'dist/css/binary.css'],
+            'dist/css/binary.min.css': ['src/css/**/*.css', 'dist/css/binary.css'],        
             'dist/css/binary_rtl.min.css': ['src/css/**/*.css', 'dist/css/binary_rtl.css']
         }
     }
