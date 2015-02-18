1dcf7794d7acc86ed965bdac35f5a7b55d67e3cf
Boris Yankov <borisyankov@gmail.com>
2015-02-18 14:32:30 +0200

    Update main navigation with classing branding

--- build/cssmin.js
+++ build/cssmin.js
@@ -1,8 +1,8 @@
 module.exports = {
     all: {
         files: {
-            '../tech-blog/binary.min.css': ['src/css/**/*.css', 'dist/css/binary.css'],
-            'dist/css/binary.min.css': ['src/css/**/*.css', 'dist/css/binary.css'],        
+            '../../tech-blog/binary.min.css': ['src/css/**/*.css', 'dist/css/binary.css'],
+            'dist/css/binary.min.css': ['src/css/**/*.css', 'dist/css/binary.css'],
             'dist/css/binary_rtl.min.css': ['src/css/**/*.css', 'dist/css/binary_rtl.css']
         }
     }
